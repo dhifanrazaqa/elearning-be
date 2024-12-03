0e0497634b3abe298fbb96773d5e61f205a9c61e
dhifanrazaqa <dhifanofficial@gmail.com>
2024-12-03 23:35:31 +0700

    set cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://lentera-fe.vercel.app",
     credentials: true,
     allowedHeaders: [
       "Origin",
